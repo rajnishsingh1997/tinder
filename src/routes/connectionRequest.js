be3ad11632f084571790605133f294aa3339de6b
Rajnish. R. Singh Rajput <srajnish235.rs@gmail.com>
2025-03-21 15:51:37 +0530

    added validation to send connection request api

--- src/routes/connectionRequest.js
+++ src/routes/connectionRequest.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const userAuthCheckMiddleWare = require("../Middleware/authMiddleware");
-const ConnectionSchema = require("../model/connectionRequestModal");
-const { validateStatus } = require("../utils/validationConnectionRequest");
+const ConnectionModal = require("../model/connectionRequestModal");
+const {
+  validateStatus,
+  validateUser,
+} = require("../utils/validationConnectionRequest");
 const connectionRequestRouter = express.Router();
 
 connectionRequestRouter.post(
@@ -12,14 +15,34 @@ connectionRequestRouter.post(
       const { status, toUserId } = req.params;
       const { _id } = req.user;
       const isStatusValid = validateStatus(status);
+      const isUserIdValid = await validateUser(toUserId);
+
       if (!isStatusValid) {
         return res.status(400).json({
           message: "invalid status code",
         });
       }
 
+      if (!isUserIdValid) {
+        return res.status(400).json({
+          message: "unable to send request",
+        });
+      }
 
-      const newConnectionRequest = ConnectionSchema({
+      const existingConnection = await ConnectionModal.findOne({
+        $or: [
+          { fromUserId: _id, toUserId: toUserId },
+          { fromUserId: toUserId, toUserId: _id },
+        ],
+      });
+
+      if (existingConnection) {
+        return res.status(400).json({
+          message: "Connection request already exits",
+        });
+      }
+
+      const newConnectionRequest = ConnectionModal({
         fromUserId: _id,
         toUserId: toUserId,
         status: status,
--- src/utils/validationConnectionRequest.js
+++ src/utils/validationConnectionRequest.js
@@ -1,6 +1,13 @@
+const UserModal = require("../model/user");
+const mongoose = require("mongoose");
+
 function validateStatus(str) {
   const allowedStatus = ["ignored", "interested"];
   return allowedStatus.includes(str);
 }
 
-module.exports = { validateStatus };
+async function validateUser(userId) {
+  return  mongoose.Types.ObjectId.isValid(userId);
+}
+
+module.exports = { validateStatus, validateUser };
